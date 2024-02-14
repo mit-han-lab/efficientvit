9340c1d3e3f4bc54111f5720420c80a35c3fdbff
Han Cai <hancai@mit.edu>
2024-02-13 20:42:35 -0500

    clean

--- assets/files/eval_sam_model.sh
+++
@@ -1,8 +0,0 @@
-# COCO-Val2017: all=74.454, large=81.410, medium=77.201, small=68.159
-python eval_sam_coco.py --model l0
-
-# COCO-Val2017: all=75.183, large=81.786, medium=78.110, small=68.944
-python eval_sam_coco.py --model l1
-
-# COCO-Val2017: all=75.656, large=81.706, medium=78.644, small=69.689
-python eval_sam_coco.py --model l2
