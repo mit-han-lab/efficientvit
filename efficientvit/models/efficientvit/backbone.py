0049597dd9a4a91af80345b1cccd252c613ae9ec
HONG,HAO-SYUAN <pg785632541@gmail.com>
2024-09-14 19:52:15 +0800

    fix: adjust input channels in stage0 initialization for dynamic adaptability

--- efficientvit/models/efficientvit/backbone.py
+++ efficientvit/models/efficientvit/backbone.py
@@ -221,7 +221,7 @@ class EfficientViTLargeBackbone(nn.Module):
         # stage 0
         stage0 = [
             ConvLayer(
-                in_channels=3,
+                in_channels=in_channels,
                 out_channels=width_list[0],
                 stride=2,
                 norm=norm,
